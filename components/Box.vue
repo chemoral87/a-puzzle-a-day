edd6e6cbf088aade31d586bac5dbb442bdbd81a7
Sergio Morales <chemoral87@hotmail.com>
2022-09-06 00:30:08 -0500

    initial

---
+++ components/Box.vue
@@ -0,0 +1,122 @@
+<template>
+  <div>
+    <div v-for="(piece, ix) in pieces" :key="ix">
+      <table class="sample">
+        <tr v-for="(row, iy) in piece.shape" :key="ix + iy">
+          <td
+            v-for="(cll, iz) in row"
+            :key="ix + iy + iz"
+            :style="{ backgroundColor: cll == 1 ? piece.color : '' }"
+          >
+            {{ cll == 1 ? piece.name : '' }}
+          </td>
+        </tr>
+      </table>
+      <br />
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      pieces: [
+        {
+          shape: [
+            [1, 1, 0, 0],
+            [0, 1, 1, 1],
+          ],
+          name: 'a',
+          color: 'red',
+        },
+        {
+          shape: [
+            [1, 1, 1, 1],
+            [1, 1, 1, 1],
+          ],
+          name: 'b',
+          color: 'blue',
+        },
+        {
+          shape: [
+            [1, 1, 0],
+            [0, 1, 0],
+            [0, 1, 1],
+          ],
+          name: 'c',
+          color: 'green',
+        },
+        {
+          shape: [
+            [0, 0, 1],
+            [0, 0, 1],
+            [1, 1, 1],
+          ],
+          name: 'd',
+          color: 'pink',
+        },
+        {
+          shape: [
+            [0, 1, 1],
+            [1, 1, 1],
+          ],
+          name: 'e',
+          color: 'orange',
+        },
+        {
+          shape: [
+            [0, 0, 1, 0],
+            [1, 1, 1, 1],
+          ],
+          name: 'f',
+          color: 'purple',
+        },
+        {
+          shape: [
+            [1, 0, 1],
+            [1, 1, 1],
+          ],
+          name: 'g',
+          color: 'violet',
+        },
+        {
+          shape: [
+            [0, 0, 0, 1],
+            [1, 1, 1, 1],
+          ],
+          name: 'h',
+          color: 'DarkTurquoise',
+        },
+      ],
+    }
+  },
+  // https://htmlcolorcodes.com/es/nombres-de-los-colores/
+  methods: {},
+  mounted() {
+    let me = this
+  },
+}
+</script>
+<style scoped>
+table.sample {
+  border-style: none;
+  border-collapse: collapse;
+}
+table.sample th {
+  border-width: 2px;
+  width: 10px;
+  height: 10px;
+  padding: 5px;
+  border-style: inset;
+  border-color: white;
+}
+table.sample td {
+  width: 32px;
+  height: 32px;
+  border-width: 0px;
+  padding: 5px;
+  border-style: inset;
+  border-color: white;
+}
+</style>
---
+++ components/PuzzleBoard.vue
@@ -0,0 +1,50 @@
+<template>
+  <div>
+    <div>Puzzle Board</div>
+    <table class="sample">
+      <tr v-for="(row, ix) in board" :key="ix">
+        <td v-for="(cll, iy) in row" :key="ix + iy">{{ cll }}</td>
+      </tr>
+    </table>
+  </div>
+</template>
+<script>
+export default {
+  props: {},
+  data() {
+    return {
+      board: [
+        ['Jan', 'Feb', 'Mar', 'Apr', 'May', 'Jun'],
+        ['Jul', 'Aug', 'Sep', 'Oct', 'Nov', 'Dec'],
+        [1, 2, 3, 4, 5, 6, 7],
+        [8, 9, 10, 11, 12, 13, 14],
+        [15, 16, 17, 18, 19, 20, 21],
+        [22, 23, 24, 25, 26, 27, 28],
+        [29, 30, 31],
+      ],
+    }
+  },
+  methods: {},
+  mounted() {
+    let me = this
+  },
+}
+</script>
+<style scoped>
+table.sample {
+  border-style: none;
+  border-collapse: collapse;
+}
+table.sample th {
+  border-width: 2px;
+  padding: 5px;
+  border-style: inset;
+  border-color: white;
+}
+table.sample td {
+  border-width: 2px;
+  padding: 5px;
+  border-style: inset;
+  border-color: white;
+}
+</style>
