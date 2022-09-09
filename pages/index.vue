3dc998ec2792b5247b662198b1092d8e0cfc7796
Sergio Morales <chemoral87@hotmail.com>
2022-09-08 19:54:38 -0500

    button

--- pages/index.vue
+++ pages/index.vue
@@ -10,7 +10,7 @@
         </v-card-text>
         <v-card-actions>
           <v-spacer />
-          <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn>
+          <!-- <v-btn color="primary" nuxt to="/inspire"> Continue </v-btn> -->
         </v-card-actions>
       </v-card>
     </v-col>
