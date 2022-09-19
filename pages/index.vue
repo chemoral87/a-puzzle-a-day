7941412d35b5aba1d126d5c69504ec1b6d54cfc7
Sergio Morales <chemoral87@hotmail.com>
2022-09-18 23:10:42 -0500

    autoresolve

--- pages/index.vue
+++ pages/index.vue
@@ -1,13 +1,13 @@
 <template>
-  <v-row justify="center" align="center">
-    <v-col>
-      <span class="headline">A Puzzle a Day </span>
-    </v-col>
-    <v-col cols="12" sm="12" md="10">
-      <PuzzleBoard />
-    </v-col>
-    <v-col cols="12" sm="12" md="10">
-      <Box />
-    </v-col>
-  </v-row>
+  <div>
+    <span class="headline">A Puzzle a Day </span>
+    <v-row>
+      <v-col cols="12" sm="12" md="10">
+        <PuzzleBoard />
+      </v-col>
+      <v-col cols="12" sm="12" md="10">
+        <Box />
+      </v-col>
+    </v-row>
+  </div>
 </template>
